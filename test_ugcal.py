4f1c5c0ddeca5c45d3971193399a8b7922d7ea03
Povilas Balzaravicius Pawka <povilas@uber.com>
2016-03-31 10:46:49 +0300

    Added event fixture, refactored tests

--- test_ugcal.py
+++ test_ugcal.py
@@ -7,59 +7,121 @@ from ugcal import UGCal
 @pytest.fixture
 def php_meetup():
     return {
-        u'status': u'upcoming',
-        u'updated': 1457875805000,
-        u'group': {
-            u'who': u'PHPers',
-            u'name': u'Vilnius PHP Meetups',
-            u'join_mode': u'open',
-            u'created': 1416565027000,
-            u'lon': 25.270000457763672,
-            u'lat': 54.70000076293945,
-            u'urlname': u'vilniusphp',
-            u'id': 18204759
+        'status': 'upcoming',
+        'updated': 1457875805000,
+        'group': {
+            'who': 'PHPers',
+            'name': 'Vilnius PHP Meetups',
+            'join_mode': 'open',
+            'created': 1416565027000,
+            'lon': 25.270000457763672,
+            'lat': 54.70000076293945,
+            'urlname': 'vilniusphp',
+            'id': 18204759
         },
-        u'name': u'VilniusPHP 0x29',
-        u'created': 1418629034000,
-        u'venue': {
-            u'city': u'Vilnius',
-            u'name': u'\u0160MTP',
-            u'repinned': False,
-            u'lon': 25.294130325317383,
-            u'localized_country_name': u'Lithuania',
-            u'address_1': u'J. Galvyd\u017eio g. 5, Vilnius',
-            u'country': u'lt',
-            u'lat': 54.710880279541016,
-            u'id': 23491895
+        'name': 'VilniusPHP 0x29',
+        'created': 1418629034000,
+        'venue': {
+            'city': 'Vilnius',
+            'name': '\u0160MTP',
+            'repinned': False,
+            'lon': 25.294130325317383,
+            'localized_country_name': 'Lithuania',
+            'address_1': 'J. Galvyd\u017eio g. 5, Vilnius',
+            'country': 'lt',
+            'lat': 54.710880279541016,
+            'id': 23491895
         },
-        u'utc_offset': 10800000,
-        u'visibility': u'public',
-        u'yes_rsvp_count': 5,
-        u'link': u'http://www.meetup.com/vilniusphp/events/228864161/',
-        u'time': 1460043000000,
-        u'duration': 9000000,
-        u'waitlist_count': 0,
-        u'id': u'gnbddlyvgbkb',
-        u'description': u"<p>Speakers: TBA</p> <p>Do you have what to share with community? Feel free to contact us and we'll register you as a speaker!</p> "  # noqa
+        'utc_offset': 10800000,
+        'visibility': 'public',
+        'yes_rsvp_count': 5,
+        'link': 'http://www.meetup.com/vilniusphp/events/228864161/',
+        'time': 1460043000000,
+        'duration': 9000000,
+        'waitlist_count': 0,
+        'id': 'gnbddlyvgbkb',
+        'description': u"<p>Speakers: TBA</p> <p>Do you have what to share with community? Feel free to contact us and we'll register you as a speaker!</p> "  # noqa
     }
 
 
-def test_existing_events_by_links():
-    php_meetup = {
-        'name': 'Vilnius PHP 123',
-        'link': 'http://www.meetup.com/php_meetup/events/123456/'
+@pytest.fixture
+def js_meetup():
+    return {
+        'status': 'upcoming',
+        'updated': 1457875805000,
+        'name': 'Vilnius JS',
+        'created': 1418629034000,
+        'utc_offset': 10800000,
+        'visibility': 'public',
+        'yes_rsvp_count': 5,
+        'link': 'http://www.meetup.com/js_meetup/events/654321/',
+        'time': 1460043000000,
+        'duration': 9000000,
+        'waitlist_count': 0,
+        'id': 'gnbddlyvgbkb',
+        'description': u"<p>Speakers: TBA</p> <p>Do you have what to share with community? Feel free to contact us and we'll register you as a speaker!</p> "  # noqa
     }
-    js_meetup = {
-        'name': 'JS Meetup',
-        'link': 'http://www.meetup.com/js_meetup/events/654321/'
+
+
+@pytest.fixture
+def php_event():
+    return {
+        'created': '2015-07-29T19:21:09.000Z',
+        'creator': {
+             'displayName': 'Povilas Balzaravi\u010dius',
+             'email': 'pavvka@gmail.com'
+        },
+        'description': 'Daugiau informacijos: http://www.vilniusphp.lt/\n\nRSVP: http://www.meetup.com/vilniusphp/events/228864161/',  # noqa
+        'end': {
+            'dateTime': '2016-04-07T21:00:00+03:00'
+        },
+        'etag': '"2915751232820000"',
+        'hangoutLink':
+        'https://plus.google.com/hangouts/_/calendar/bGhjdWE1cHFkcmhrYmg2amU4NTFjM21lZmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ.d425a6jmpbqunk8qadussb19bg',  # noqa
+        'htmlLink': 'https://www.google.com/calendar/event?eid=ZDQyNWE2am1wYnF1bms4cWFkdXNzYjE5YmdfMjAxNjA0MDdUMTYwMDAwWiBsaGN1YTVwcWRyaGtiaDZqZTg1MWMzbWVmZ0Bn',  # noqa
+        'iCalUID': 'd425a6jmpbqunk8qadussb19bg@google.com',
+        'id': 'd425a6jmpbqunk8qadussb19bg_20160407T160000Z',
+        'kind': 'calendar#event',
+        'organizer': {
+            'displayName': 'LTU Meetups - usergroups.lt',
+            'email': 'lhcua5pqdrhkbh6je851c3mefg@group.calendar.google.com',
+            'self': True
+        },
+        'originalStartTime': {
+            'dateTime': '2016-04-07T19:00:00+03:00'
+        },
+        'recurringEventId': 'd425a6jmpbqunk8qadussb19bg',
+        'reminders': {
+            'useDefault': True
+        },
+        'sequence': 1,
+        'start': {
+            'dateTime': '2016-04-07T19:00:00+03:00'
+        },
+        'status': 'confirmed',
+        'summary': 'Vilnius PHP',
+        'updated': '2016-03-13T13:26:56.410Z'
     }
+
+
+def test_existing_events_by_links(php_meetup, js_meetup):
+    php_meetup['name'] = 'Vilnius PHP 123'
+    php_meetup['link'] = 'http://www.meetup.com/php_meetup/events/123456/'
 
     php_event = {
+        'id': 'qwerty',
         'summary': 'Some event',
+        'start': {
+            'dateTime': '2016-03-30 22:00'
+        },
         'description': 'lorem ipsum http://www.meetup.com/php_meetup/events/123456/ somehting else.'  # noqa
     }
     ruby_event = {
+        'id': 'azerty',
         'summary': 'Ruby',
+        'start': {
+            'dateTime': '2016-03-31 22:00'
+        },
         'description': ''
     }
 
@@ -72,47 +134,53 @@ def test_existing_events_by_links():
     } == result
 
 
-def test_existing_events_by_names():
-    php_meetup = {
-        'link': 'http://www.meetup.com/php_meetup/events/123456/',
-        'name': 'Vilnius PHP 123'
-    }
-    js_meetup = {
-        'link': 'http://www.meetup.com/js_meetup/events/654321/',
-        'name': 'JS Meets Meteor!'
-    }
+# def test_existing_events_by_names(php_meetup, js_meetup):
+#     php_meetup['link'] = 'http://www.meetup.com/php_meetup/events/123456/'
+#     php_meetup['name'] = 'Vilnius PHP 123'
+#
+#     js_meetup['link'] = 'http://www.meetup.com/js_meetup/events/654321/'
+#     js_meetup['name'] = 'JS Meets Meteor!'
+#
+#     existing_php_event = {
+#         'id': 'qwerty',
+#         'summary': 'Vilnius PHP 123',
+#         'start': {
+#             'dateTime': '2016-04-07T18:30:00+03:00'
+#         }
+#     }
+#     other_php_event = {
+#         'id': 'qwerty',
+#         'summary': 'Vilnius PHP Woo',
+#         'start': {
+#             'dateTime': '2016-03-07T18:30:00+03:00'
+#         }
+#     }
+#     ruby_event = {
+#         'id': 'azerty',
+#         'summary': 'Ruby',
+#         'start': {
+#             'dateTime': '2016-03-31 22:00'
+#         },
+#         'description': ''
+#     }
+#
+#     meetups = [php_meetup, js_meetup]
+#     gcal_events = [other_php_event, existing_php_event, ruby_event]
+#
+#     result = UGCal.find_existing_events(meetups, gcal_events)
+#     assert {
+#         'http://www.meetup.com/php_meetup/events/123456/': existing_php_event
+#     } == result
 
-    existing_php_event = {'summary': 'Vilnius PHP 123'}
-    other_php_event = {'summary': 'Vilnius PHP Woooo'}
-    ruby_event = {'summary': 'Ruby 4 eva'}
 
-    meetups = [php_meetup, js_meetup]
-    gcal_events = [other_php_event, existing_php_event, ruby_event]
+def test_existing_events_by_name_begining(php_meetup, php_event):
+    php_event['summary'] = php_meetup['name'][:10]
+    php_event['description'] = ''
 
-    result = UGCal.find_existing_events(meetups, gcal_events)
-    assert {
-        'http://www.meetup.com/php_meetup/events/123456/': existing_php_event
-    } == result
-
-
-def test_existing_events_by_name_begining():
-    php_meetup = {
-        'link': 'http://www.meetup.com/php_meetup/events/123456/',
-        'time': 1460043000000,
-        'name': 'Vilnius PHP 123'
-    }
-
-    existing_php_event = {
-        'summary': 'Vilnius PHP',
-        'time': 1460043000000,
-    }
-
-    meetups = [php_meetup]
-    gcal_events = [existing_php_event]
-    result = UGCal.find_existing_events(meetups, gcal_events)
-    assert {
-        'http://www.meetup.com/php_meetup/events/123456/': existing_php_event
-    } == result
+    result = UGCal.find_existing_events([php_meetup], [php_event])
+    assert 1 == len(result)
+    assert php_meetup['link'] in result
+    assert php_event == result[php_meetup['link']]
 
 
 def test_filter_events_to_create():
